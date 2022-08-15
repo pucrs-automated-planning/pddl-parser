a2b9389d7031091d2455d3176614285c9ca279fb
Mau Magnaguagno <maumagnaguagno@gmail.com>
2022-08-15 13:54:38 -0300

    Fill setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from distutils.core import setup
 
 setup(
     name="pddl_parser",
-    version="1.0",
-    packages=["pddl_parser"],
+    description="Classical Planning in Python"
+    version="1.2",
+    author="Mau Magnaguagno",
+    url="https://github.com/pucrs-automated-planning/pddl-parser",
+    packages=["pddl_parser"]
+    license="GPLv3"
 )
